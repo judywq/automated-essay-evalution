08e826f3e7cec9c9420d86847d95e84e71c152e1
Judy Wang <wangqiao890618@gmail.com>
2023-10-22 19:09:51 +0900

    Prepare essay data.

--- lib/essay.py
+++ lib/essay.py
@@ -1,35 +1,42 @@
 from enum import Enum
 import os
 import logging
+from lib.io import read_data
 
 logger = logging.getLogger(__name__)
 
 
 class Level(Enum):
-    NONE = 'none'
-    LOW = 'low'
-    MEDIUM = 'medium'
-    HIGH = 'high'
+    NONE = "none"
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
 
 
 class Essay:
-    TRUNCATE_LEN = 30
-    def __init__(self, fn, text) -> None:
+    TRUNCATE_LEN = 50
+
+    def __init__(self, fn, text, prompt="", level=Level.NONE) -> None:
         self.fn = fn
         self.text = text
-        self.level = Level.NONE
-    
+        self.prompt = prompt
+        self.level = level
+
     def __str__(self) -> str:
-        txt = (self.text[:self.TRUNCATE_LEN] + '..') if len(self.text) > self.TRUNCATE_LEN else self.text
-        return f"Essay({self.fn}, {txt})"
-    
+        txt = (
+            (self.text[: self.TRUNCATE_LEN] + "..")
+            if len(self.text) > self.TRUNCATE_LEN
+            else self.text
+        )
+        return f"Essay({self.fn}, {self.prompt}, {self.level.value}, {txt})"
+
     def __repr__(self) -> str:
         return self.__str__()
-    
+
     @property
     def level(self):
         return self._level
-    
+
     @level.setter
     def level(self, level):
         if isinstance(level, str):
@@ -40,12 +47,29 @@ class Essay:
                 level = Level.NONE
         self._level = level
 
+    # @classmethod
+    # def load_essays(cls, folder_path, ext='.txt'):
+    #     essays = []
+    #     for filename in os.listdir(folder_path):
+    #         if filename.endswith(ext):
+    #             with open(os.path.join(folder_path, filename)) as file:
+    #                 e = cls(filename, file.read())
+    #                 essays.append(e)
+    #     return essays
+
     @classmethod
-    def load_essays(cls, folder_path, ext='.txt'):
-        essays = []
-        for filename in os.listdir(folder_path):
-            if filename.endswith(ext):
-                with open(os.path.join(folder_path, filename)) as file:
-                    e = cls(filename, file.read())
-                    essays.append(e)
-        return essays
+    def load_essays(cls, index_file, essay_root):
+        def create_object(row):
+            with open(os.path.join(essay_root, row["Filename"])) as file:
+                text = file.read().strip()
+            obj = cls(
+                row["Filename"],
+                text=text,
+                prompt=row["Prompt"],
+                level=row["Score Level"],
+            )
+            return obj
+
+        df = read_data(index_file)
+        objs = df.apply(create_object, axis=1).tolist()
+        return objs
--- setting.py
+++ setting.py
@@ -1,15 +1,60 @@
 import os
+import datetime
+
+uploaded_file_id_fn = 'file-id-eassy-test-1-2023-10-22-16-44-03.json'
+
+date_str = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+model_suffix = 'eassy-test-1'
 
 input_root= 'data/input/AWE'
 index_path = os.path.join(input_root, 'index', 'index.csv')
 
+essay_root = os.path.join(input_root, 'responses', 'original')
+
 output_root = 'data/output'
-train_path = os.path.join(output_root, 'train.csv')
-val_path = os.path.join(output_root, 'val.csv')
-test_path = os.path.join(output_root, 'test.csv')
+
+index_train_filename = os.path.join(output_root, 'index', 'train.csv')
+index_val_filename = os.path.join(output_root, 'index', 'val.csv')
+index_test_filename = os.path.join(output_root, 'index', 'test.csv')
+
+dataset_train_filename = os.path.join(output_root, 'dataset', 'train.jsonl')
+dataset_val_filename = os.path.join(output_root, 'dataset', 'val.jsonl')
+dataset_test_filename = os.path.join(output_root, 'dataset', 'test.jsonl')
+
+file_id_filename = os.path.join(output_root, 'ids', f'file-id-{model_suffix}-{date_str}.json')
+model_id_filename = os.path.join(output_root, 'ids', f'model-id-{model_suffix}-{date_str}.json')
+uploaded_file_id_filename = os.path.join(output_root, 'ids', uploaded_file_id_fn)
 
 num_per_group = {
     'train': 30,
     'val': 10,
     'test': 10
 }
+
+# num_per_group = {
+#     'train': 10,
+#     'val': 2,
+#     'test': 10
+# }
+
+num_of_essays_per_prompt = 5
+
+
+system_message = """You are a language expert who evaluate user's input with 3 levels (low/medium/high), based on the following rubrics:
+An essay with a "high" level accomplishes at least the following:
+* Addresses the topic and task well, though some points may not be fully elaborated.
+* Is generally well organized and well developed, using clearly appropriate and sufficient explanations, exemplifications, and/or details.
+* Displays unity, progression, and coherence., though there may be occasional redundancy, digression, or unclear connections.
+* Demonstrates facility in the use of language, showcasing syntactic variety, appropriate word choice in a good range of vocabulary, and idiomaticity. While minor lexical or grammatical errors might be present, they do not interfere with meaning.
+
+An essay with a "medium" level is marked by one or more of the following:
+* Generally addresses the topic and task.
+* Offers somewhat developed explanations, exemplifications, and/or details, though there might be moments where elaboration is lacking or slightly off-target.
+* Displays unity, progression, and coherence for the most part, but the connection of ideas may be occasionally obscured or lack smooth transitions.
+* Demonstrates the ability to use certain syntactic structures and vocabulary, but the range of these structures and vocabulary is limited. There might be inconsistencies in sentence formation and word choice leading to occasional lack of clarity or slightly obscured meaning.
+
+An essay with a "low" level may reveal one or more of the following weaknesses:
+* Limited development in response to the topic and task or serious disorganization or underdevelopment.
+* Inadequate organization or connection of ideas, with little or no detail, irrelevant specifics, or questionable responsiveness to the task.
+* Inappropriate or insufficient exemplifications, explanations, or details to support or illustrate generalizations.
+* A noticeably inappropriate choice of words, word forms, or serious and frequent errors in sentence structure or usage."""
