dc51f77f6d2f97bdb7f7f71973f27c470965ccbc
Judy Wang <wangqiao890618@gmail.com>
2023-10-22 15:00:26 +0900

    Split data into train, val, test set.

--- setting.py
+++ setting.py
@@ -1,1 +1,15 @@
-index_path = 'data/input/index.csv'
+import os
+
+input_root= 'data/input/AWE'
+index_path = os.path.join(input_root, 'index', 'index.csv')
+
+output_root = 'data/output'
+train_path = os.path.join(output_root, 'train.csv')
+val_path = os.path.join(output_root, 'val.csv')
+test_path = os.path.join(output_root, 'test.csv')
+
+num_per_group = {
+    'train': 30,
+    'val': 10,
+    'test': 10
+}
