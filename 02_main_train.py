230cec3571cfaf027af31fa87da9816fa7db4d22
Judy <wangqiao890618@gmail.com>
2023-12-04 01:07:41 +0900

    Limit running job.

--- 02_main_train.py
+++ 02_main_train.py
@@ -6,6 +6,7 @@ from configlist import config_list
 
 
 def main():
+    running_count = 0
     for config_files in config_list:
         config = MyConfig(file_paths=config_files)
 
@@ -15,6 +16,12 @@ def main():
         job = finetuner.retrieve_job()
         if job:
             ic(job.status, job.id)
+            if job.status in ["running", "validating_files"]:
+                running_count += 1
+        
+        if running_count >= 3:
+            ic("Too many running jobs, quit.")
+            break
 
 
 if __name__ == "__main__":
