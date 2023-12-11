c85f939c092b7b4426bd462cab7bb785d9d2145e
Judy <wangqiao890618@gmail.com>
2023-12-11 21:39:57 +0900

    Add support for temperature variable

--- configlist.py
+++ configlist.py
@@ -19,6 +19,8 @@ config_unit_gpt_3_5_turbo = "./configs/config.gpt3.5-turbo.json"
 config_unit_gpt_4_turbo = "./configs/config.gpt4-turbo.json"
 
 config_unit_20231202 = "./configs/config.20231202.json"
+config_unit_20231203 = "./configs/config.20231203.json"
+config_unit_20231208 = "./configs/config.20231208.json"
 
 config_set_fa_float = [config_unit_base, config_unit_form_all, config_unit_float]
 config_set_f1_float = [config_unit_base, config_unit_form_1, config_unit_float]
@@ -29,17 +31,17 @@ config_set_f2_int = [config_unit_base, config_unit_form_2, config_unit_int]
 
 config_list = [
     config_set_f1_float,
-    config_set_f1_int,
+    # config_set_f1_int,
     config_set_f2_float,
-    config_set_f2_int,
+    # config_set_f2_int,
     config_set_fa_float,
-    config_set_fa_int,
+    # config_set_fa_int,
 ]
 
-ADD_20231202 = False
-ADD_20231202 = True
+ADD_20231208 = False
+ADD_20231208 = True
 
-if ADD_20231202:
+if ADD_20231208:
     for confitset in config_list:
-        confitset.append(config_unit_20231202)
+        confitset.append(config_unit_20231208)
     
